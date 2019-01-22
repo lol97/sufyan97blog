50c00dbe090e0af565719cc72cc6ee1c0833ad84
aglis-sae <blackmanga80@gmail.com>
2019-01-23 00:36:30 +0700

    add stegano

--- Python/stegano_learn.py
+++ Python/stegano_learn.py
@@ -1,5 +1,21 @@
+'''
+    Stegano Code
+    with
+        library stegano
+    sufyan97
+    install stegano
+    pip install stegano
+'''
+#import lsb method
 from stegano import lsb
-secret = lsb.hide("car.png","hai")
-secret.save("car-sec.png")
 
-print(lsb.reveal("car-sec.png"))
+# insert message
+secret = lsb.hide("cat.png","hai")
+
+#save image
+secret.save("cat-sec.png")
+
+#show message
+print(lsb.reveal("cat-sec.png"))
+
+#THANKS
